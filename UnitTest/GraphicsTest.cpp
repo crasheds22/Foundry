69d33a0e654c29644ea30b8a638646b6d2214e1a
Aaron <a.thomson22@outlook.com>
2020-08-23 20:25:51 +0800

    Camera test and cubes

--- UnitTest/GraphicsTest.cpp
+++ UnitTest/GraphicsTest.cpp
@@ -2,10 +2,22 @@
 
 #include <iostream>
 
-void ProcessInput(GLFWwindow* window)
+void ProcessInput(GLFWwindow* window, CameraTest* camera = nullptr, float dt = 0.0f)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (camera)
+    {
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+            camera->MoveCamera(Camera_Movement::FORWARD, dt);
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+            camera->MoveCamera(Camera_Movement::LEFT, dt);
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+            camera->MoveCamera(Camera_Movement::BACKWARD, dt);
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+            camera->MoveCamera(Camera_Movement::RIGHT, dt);
+    }
 }
 
 class ShaderTest
@@ -36,7 +48,36 @@ bool GraphicsTest::Test()
 
     allPassed = Textures();
 
+    allPassed = CameraAndCubes();
+
     return allPassed;
+}
+
+void GraphicsTest::MouseCallback(GLFWwindow* window, double xPos, double yPos)
+{
+    GraphicsTest* This = (GraphicsTest*)glfwGetWindowUserPointer(window);
+
+    if (This->FirstMouse)
+    {
+        This->lastX = xPos;
+        This->lastY = yPos;
+        This->FirstMouse = false;
+    }
+
+    float xOff = xPos - This->lastX;
+    float yOff = This->lastY - yPos;
+
+    This->lastX = xPos;
+    This->lastY = yPos;
+
+    This->camera.RotateCamera(xOff, yOff);
+}
+
+void GraphicsTest::ScrollCallback(GLFWwindow* window, double xOff, double yOff)
+{
+    GraphicsTest* This = (GraphicsTest*)glfwGetWindowUserPointer(window);
+
+    This->camera.ChangeZoom(yOff);
 }
 
 bool GraphicsTest::HelloWindow()
@@ -465,6 +506,212 @@ bool GraphicsTest::Textures()
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+
+    return true;
+}
+
+bool GraphicsTest::CameraAndCubes()
+{
+    camera = CameraTest(glm::vec3(0.0f, 0.0f, 3.0f));
+
+    deltaTime = 0.0f;
+    lastFrame = 0.0f;
+    lastX = 800.0f / 2.0f;
+    lastY = 500.0f / 2.0f;
+    FirstMouse = true;
+
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(800, 500, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetWindowUserPointer(window, this);
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* win, int w, int h) {glViewport(0, 0, w, h); });
+    glfwSetCursorPosCallback(window, GraphicsTest::MouseCallback);
+    glfwSetScrollCallback(window, GraphicsTest::ScrollCallback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile our shader zprogram
+    // ------------------------------------
+    ShaderTest ourShader("../Data/Shaders/7.4.camera.vs", "../Data/Shaders/7.4.camera.fs");
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+    // world space positions of our cubes
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f,  2.0f, -2.5f),
+        glm::vec3(1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+    // load and create a texture 
+    // -------------------------
+    TextureTest texture1("../Data/Textures/container.jpg", true);
+    TextureTest texture2("../Data/Textures/awesomeface.png", true, 4);
+
+    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+    // -------------------------------------------------------------------------------------------
+    Shader::Use(ourShader.ID);
+    Shader::setInt(ourShader.ID, "texture1", 0);
+    Shader::setInt(ourShader.ID, "texture2", 1);
+
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        // -----
+        ProcessInput(window, &camera, deltaTime);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1.ID);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2.ID);
+
+        // activate shader
+        Shader::Use(ourShader.ID);
+
+        // pass projection matrix to shader (note that in this case it could change every frame)
+        glm::mat4 projection = glm::perspective(glm::radians(camera.GetZoom()), (float)800 / (float)500, 0.1f, 100.0f);
+        Shader::setMat4(ourShader.ID, "projection", projection);
+
+        // camera/view transformation
+        glm::mat4 view = camera.ViewMatrix();
+        Shader::setMat4(ourShader.ID, "view", view);
+
+        // render boxes
+        glBindVertexArray(VAO);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            Shader::setMat4(ourShader.ID, "model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
--- UnitTest/GraphicsTest.h
+++ UnitTest/GraphicsTest.h
@@ -1,11 +1,112 @@
 #pragma once
 #include "UnitTest.h"
 
+#include <map>
+#include <functional>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "../Graphics/Shader.h"
 #include "../Graphics/Texture.h"
+
+enum class Camera_Movement { FORWARD, BACKWARD, RIGHT, LEFT };
+
+class CameraTest
+{
+public:
+    CameraTest(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = -90.0f, float pitch = 0.0f) 
+        : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MoveSpeed(2.5f), MouseSensitivity(0.1f), Zoom(45.0f)
+    {
+        Position = position;
+        WorldUp = up;
+        Yaw = yaw;
+        Pitch = pitch;
+
+        UpdateCameraVectors();
+
+        MovementMapper = { {Camera_Movement::FORWARD, Front}, {Camera_Movement::BACKWARD, -Front}, {Camera_Movement::RIGHT, Right}, {Camera_Movement::LEFT, -Right} };
+    }
+
+    glm::mat4 ViewMatrix()
+    {
+        return glm::lookAt(Position, Position + Front, Up);
+    }
+
+    glm::vec3 GetPosition() const { return Position; }
+    glm::vec3 GetRight() const { return Right; }
+    glm::vec3 GetFront() const { return Front; }
+    float GetZoom() const { return Zoom; }
+
+    void MoveCamera(Camera_Movement Direction, float deltaTime)
+    {
+        float velocity = MoveSpeed * deltaTime;
+
+        Position += MovementMapper[Direction] * velocity;
+    }
+    void RotateCamera(float xOff, float yOff, bool constrainPitch = true)
+    {
+        xOff *= MouseSensitivity;
+        yOff *= MouseSensitivity;
+
+        Yaw += xOff;
+        Pitch += yOff;
+
+        if (constrainPitch)
+        {
+            if (Pitch > 89.0f)
+                Pitch = 89.0f;
+
+            if (Pitch < -89.0f)
+                Pitch = -89.0f;
+        }
+
+        UpdateCameraVectors();
+    }
+    void ChangeZoom(float yOff)
+    {
+        Zoom -= yOff;
+        if (Zoom < 1.0f)
+            Zoom = 1.0f;
+
+        if (Zoom > 45.0f)
+            Zoom = 45.0f;
+    }
+
+    std::map<Camera_Movement, glm::vec3> MovementMapper { };
+
+private:
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+
+    float Yaw;
+    float Pitch;
+
+    float MoveSpeed;
+    float MouseSensitivity;
+
+    float Zoom;
+
+    void UpdateCameraVectors()
+    {
+        glm::vec3 front;
+        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        front.y = sin(glm::radians(Pitch));
+        front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+
+        Front = glm::normalize(front);
+        Right = glm::normalize(glm::cross(Front, WorldUp));
+        Up = glm::normalize(glm::cross(Right, Front));
+
+        MovementMapper = { {Camera_Movement::FORWARD, Front}, {Camera_Movement::BACKWARD, -Front}, {Camera_Movement::RIGHT, Right}, {Camera_Movement::LEFT, -Right} };
+    }
+};
 
 class GraphicsTest :
     public UnitTest
@@ -14,11 +115,23 @@ public:
     GraphicsTest() : UnitTest("Graphics Test") { };
 
     bool Test() override;
+
+    static void MouseCallback(GLFWwindow* window, double xPos, double yPos);
+    static void ScrollCallback(GLFWwindow* window, double xOff, double yOff);
+
+    float deltaTime{};
+    float lastFrame{};
+    bool FirstMouse{};
+    float lastX{};
+    float lastY{};
+
+    CameraTest camera;
     
 private:
     bool HelloWindow();
     bool HelloTriangle();
     bool UsingShaders();
     bool Textures();
+    bool CameraAndCubes();
 };
 
