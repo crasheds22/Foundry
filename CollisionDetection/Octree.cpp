35c5de14a8dfaba25d22a2401bdf066e7e8c560c
Frosk-Kristian <55474756+Frosk-Kristian@users.noreply.github.com>
2020-09-01 14:09:43 +0800

    functions to manipulate/interact with tree
    Added functions for destroying and traversing the tree.
    Starting working on inserting nodes into the tree.

--- CollisionDetection/Octree.cpp
+++ CollisionDetection/Octree.cpp
@@ -4,3 +4,64 @@ template <class T>
 Octree<T>::Octree() {
 	root = nullptr;
 }
+
+template <class T>
+Octree<T>::~Octree() {
+	Destroy(root);
+	delete root;
+}
+
+template <class T>
+void Octree<T>::Traverse(node<T> n, std::function<void(const T&)> f) {
+	for (int i = 0; i < 8; i++) {
+		if (n.children[i] != nullptr) {
+			f(n.children[i]->info);
+		} // end if
+	} // end for
+}
+
+template <class T>
+void Octree<T>::InsertNode(node<T> in, node<T> prev) {
+	
+}
+
+template <class T>
+void Octree<T>::Insert(node<T> n) {
+	if (root == nullptr) {
+		root = n;
+	} // end if
+	else {
+		InsertNode(n, root);
+	} // end else
+}
+
+template <class T>
+void Octree<T>::Insert(T t) {
+	node<T> temp;
+	temp.info = t;
+
+	if (root == nullptr) {
+		root = temp;
+	} // end if
+	else {
+		InsertNode(temp, root);
+	} // end else
+}
+
+template <class T>
+void Octree<T>::Destroy(node<T> n) {
+	for (int i = 0; i < 8; i++) {
+		if (n.children[i] != nullptr) {
+			Destroy(n.children[i]);
+		} // end if
+		delete n.children[i];
+	} // end for
+	
+	delete n.parent;
+}
+
+template <class T>
+void Octree<T>::DestroyTree() {
+	Destroy(root);
+	delete root;
+}
--- CollisionDetection/Octree.h
+++ CollisionDetection/Octree.h
@@ -7,11 +7,14 @@
 * @author Kristian
 */
 
+#include <functional>
+#include <iostream>
+
 template <class T>
 struct node {
-	T info;
-	T* parent;
-	T* children[8];
+	T info{};
+	node* parent;
+	node* children[8];
 
 	node() : children() {
 		// initialises children to nullptr
@@ -23,12 +26,53 @@ class Octree {
 public:
 	/**
 	* @brief constructor
+	* 
 	* @post octree is constructed with root = nullptr
 	*/
 	Octree();
+
+	/**
+	* @brief destructor
+	* 
+	* @post octree is destructed
+	*/
+	~Octree();
 	
+	/**
+	* @brief function to insert node into tree
+	* 
+	* @param node<T> n - node to be inserted into tree
+	*/
+	void Insert(node<T> n);
+
+	/**
+	* @brief overload of previous insert function
+	*	creates a new node and stores t into it and then inserts the new node into the tree
+	* @param T t - value to be inserted into tree
+	*/
+	void Insert(T t);
+
+	/**
+	* @brief function to destroy tree
+	*	calls Destroy() on root
+	*/
+	void DestroyTree();
 protected:
 	node<T>* root;
 private:
-	
+	/**
+	* @brief function to traverse the tree
+	* 
+	* @param node<T> n - node of type T to start traversing from
+	* @param function f - function to perform on each node
+	*/
+	void Traverse(node<T> n, std::function<void(const T&)> f);
+	void InsertNode(node<T> in, node<T> prev);
+
+	/**
+	* @brief function to destroy node and children
+	* 
+	* @param node<T> n - node to start from
+	*/
+	void Destroy(node<T> n);
 };
