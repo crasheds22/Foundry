bf41c83f3f6a1df7cf6c0d57934af9ca55c0574d
Aaron Thomson <a.thomson22@outlook.com>
2020-09-10 15:46:06 +0800

    Shader test passed

--- Simulation/main.cpp
+++ Simulation/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 
+#include "TestBattery.h"
+
 int main()
 {
 	std::cout << "Nothing here bruv" << std::endl;
+
+	if (true)
+	{
+		TestBattery tests;
+		tests.RunTests();
+	}
+	else
+	{
+		std::cout << "unreachable" << std::endl;
+	}
 
 	return 0;
 }
