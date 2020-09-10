8a78e39db48a6dc66bb5890d8d8c046accc7f9db
Aaron <a.thomson22@outlook.com>
2020-09-10 23:18:11 +0800

    Render system

--- Simulation/sys_Render.cpp
+++ Simulation/sys_Render.cpp
@@ -1,6 +1,44 @@
 #include "sys_Render.h"
 
+#include "com_Model.h"
+#include "com_Shader.h"
+#include "com_Transform.h"
+
+#include "Coordinator.h"
+extern ECS::Coordinator gCoordinator;
+
+#include "Graphics.h"
+extern Graphics gGraphics;
+
 namespace System
 {
+    void sys_Render::Init()
+    {
+    }
 
+    void sys_Render::Update(Component::com_Camera& camera)
+    {
+        for (const auto& entity : mEntities)
+        {
+            auto& eModel = gCoordinator.GetComponent<Component::com_Model>(entity);
+            auto& eShader = gCoordinator.GetComponent<Component::com_Shader>(entity);
+            auto& eTransform = gCoordinator.GetComponent<Component::com_Transform>(entity);
+
+            eShader.Use();
+
+            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom()), gGraphics.AspectRatio(), 0.1f, 100.0f);
+            eShader.setMat4("projection", projection);
+
+            glm::mat4 view = camera.ViewMatrix();
+            eShader.setMat4("view", view);
+
+            glm::mat4 model(1.0f);
+            model = glm::translate(model, eTransform.Position());
+            
+            model = glm::scale(model, eTransform.Scale());
+            eShader.setMat4("model", model);
+
+            eModel.Draw(eShader.ID());
+        }
+    }
 }
--- Simulation/sys_Render.h
+++ Simulation/sys_Render.h
@@ -2,12 +2,16 @@
 
 #include "System.h"
 
+#include "com_Camera.h"
+
 namespace System
 {
 	class sys_Render : public ECS::System
 	{
 	public:
-		
+		void Init();
+
+		void Update(Component::com_Camera& camera);
 
 	private:
 
