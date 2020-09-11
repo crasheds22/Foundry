ea4ce9293f79d68e922838c846dd1ad52d879275
Frosk-Kristian <55474756+Frosk-Kristian@users.noreply.github.com>
2020-09-11 17:43:00 +0800

    Update ColliderTest.cpp

--- Simulation/ColliderTest.cpp
+++ Simulation/ColliderTest.cpp
@@ -15,7 +15,7 @@ bool ColliderTest::BoxBox() {
 
 	while (!a->TestCollision(aT, b, bT).HasCollision()) {
 		// move two boxes towards eachother until collision occurs
-		if (posA.x < initPosB.x || posB.x < initPosA.x) {
+		if (posA.x > initPosB.x || posB.x < initPosA.x) {
 			std::cout << "No collision detected!" << '\n';
 			return false;
 		} // end if
@@ -26,6 +26,14 @@ bool ColliderTest::BoxBox() {
 		aT->Position(posA);
 		bT->Position(posB);
 	} // end while
+
+	std::cout << posA.x << ", " << posA.y << ", " << posA.z << '\n';
+	std::cout << posB.x << ", " << posB.y << ", " << posB.z << '\n';
+
+	delete a;
+	delete aT;
+	delete b;
+	delete bT;
 
 	return true;
 } // end BoxBox
@@ -45,17 +53,22 @@ bool ColliderTest::SphereSphere() {
 
 	while (!a->TestCollision(aT, b, bT).HasCollision()) {
 		// move two spheres towards eachother until collision occurs
-		if (posA.x < initPosB.x || posB.x < initPosA.x) {
+		if (posA.x > initPosB.x || posB.x < initPosA.x) {
 			std::cout << "No collision detected!" << '\n';
 			return false;
 		} // end if
 
-		posA.x += 1.0f; // posA = glm::vec3((posA.x + 1.0f), posA.y, posA.z);
-		posB.x -= 1.0f; // posB = glm::vec3((posB.x - 1.0f), posB.y, posB.z);
+		posA.x += 1.0f;
+		posB.x -= 1.0f;
 
 		aT->Position(posA);
 		bT->Position(posB);
 	} // end while
+
+	delete a;
+	delete aT;
+	delete b;
+	delete bT;
 
 	return true;
 } // end SphereSphere
@@ -75,7 +88,7 @@ bool ColliderTest::BoxSphere() {
 
 	while (!a->TestCollision(aT, b, bT).HasCollision()) {
 		// move a box and a sphere towards eachother until collision occurs
-		if (posA.x < initPosB.x || posB.x < initPosA.x) {
+		if (posA.x > initPosB.x || posB.x < initPosA.x) {
 			std::cout << "No collision detected!" << '\n';
 			return false;
 		} // end if
@@ -87,31 +100,80 @@ bool ColliderTest::BoxSphere() {
 		bT->Position(posB);
 	} // end while
 
+	delete a;
+	delete aT;
+	delete b;
+	delete bT;
+
 	return true;
 } // end BoxSphere
 
 bool ColliderTest::BoxPlane() {
-	Component::com_Box* a = new Component::com_Box();
-	Component::com_Transform* aT = new Component::com_Transform();
-	Component::com_Plane* b = new Component::com_Plane();
-	Component::com_Transform* bT = new Component::com_Transform();
+	// initial positions
+	glm::vec3 initPosA = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 initPosB = glm::vec3(10.0f, 0.0f, 0.0f);
+	// object a
+	Component::com_Box* a = new Component::com_Box(glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+	glm::vec3 posA = initPosA; // position of object a
+	Component::com_Transform* aT = new Component::com_Transform(posA, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+	// object b
+	glm::vec3 vecB = initPosB - initPosA;
+	vecB = glm::normalize(vecB);
+	Component::com_Plane* b = new Component::com_Plane(vecB, 1.0f);
+	glm::vec3 posB = initPosB;
+	Component::com_Transform* bT = new Component::com_Transform(posB, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
 
 	while (!a->TestCollision(aT, b, bT).HasCollision()) {
 		// move a box towards a plane until collision occurs
+		if (posA.x > initPosB.x) {
+			std::cout << "No collision detected!" << '\n';
+			return false;
+		} // end if
+
+		posA.x += 1.0f;
+
+		aT->Position(posA);
 	} // end while
+
+	delete a;
+	delete aT;
+	delete b;
+	delete bT;
 
 	return true;
 } // end BoxPlane
 
 bool ColliderTest::SpherePlane() {
-	Component::com_Sphere* a = new Component::com_Sphere();
-	Component::com_Transform* aT = new Component::com_Transform();
-	Component::com_Plane* b = new Component::com_Plane();
-	Component::com_Transform* bT = new Component::com_Transform();
+	// initial positions
+	glm::vec3 initPosA = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 initPosB = glm::vec3(10.0f, 0.0f, 0.0f);
+	// object a
+	Component::com_Sphere* a = new Component::com_Sphere(glm::vec3(0.0f, 0.0f, 0.0f), 1.0f);
+	glm::vec3 posA = initPosA;
+	Component::com_Transform* aT = new Component::com_Transform(posA, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+	// object b
+	glm::vec3 vecB = initPosB - initPosA;
+	vecB = glm::normalize(vecB);
+	Component::com_Plane* b = new Component::com_Plane(vecB, 1.0f);
+	glm::vec3 posB = initPosB;
+	Component::com_Transform* bT = new Component::com_Transform(posB, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
 
 	while (!a->TestCollision(aT, b, bT).HasCollision()) {
 		// move a sphere towards a plane until collision occurs
+		if (posA.x > initPosB.x) {
+			std::cout << "No collision detected!" << '\n';
+			return false;
+		} // end if
+
+		posA.x += 1.0f;
+
+		aT->Position(posA);
 	} // end while
+
+	delete a;
+	delete aT;
+	delete b;
+	delete bT;
 
 	return true;
 } // end SpherePlane
