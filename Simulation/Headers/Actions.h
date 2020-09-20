8ffd0219ff9d1a1c0f66cfd0ee12afe1c06eab9a
Aaron Thomson <a.thomson22@outlook.com>
2020-09-20 12:57:36 +0800

    Actions

---
+++ Simulation/Headers/Actions.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace Actions
+{
+	enum class Global
+	{
+		QUIT
+		,PAUSE
+		,MENU
+		,RESUME
+	};
+
+	enum class Move
+	{
+		LEFT
+		,RIGHT
+		,FORWARD
+		,BACKWARD
+		,UP
+		,DOWN
+	};
+
+	enum class User
+	{
+		USE
+	};
+}
