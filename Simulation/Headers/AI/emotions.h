3bdcd1ce3156e8c8b6906dd3a0f53987d5b62d79
Niko Subianto <niko.subianto@gmail.com>
2020-09-22 12:01:42 +0800

    Boxbox emotions

---
+++ Simulation/Headers/AI/emotions.h
@@ -0,0 +1,14 @@
+#include <glm/glm.hpp>
+
+glm::vec4 emotions;
+glm::vec4 mood;
+
+struct personality{
+	int mPersonalityType;
+	glm::vec4 mNeutral;
+	glm::vec4 mMax;
+	glm::vec4 mMin;
+}
+
+
+	
--- Simulation/Source/Algorithm/CollisionAlgo.cpp
+++ Simulation/Source/Algorithm/CollisionAlgo.cpp
@@ -50,7 +50,38 @@ CollisionPoint CollisionAlgo::FindSpherePlane(const Component::com_Sphere* sA, c
 
 CollisionPoint CollisionAlgo::FindBoxBox(const Component::com_Box* bA, const Component::com_Transform* tA, const Component::com_Box* bB, const Component::com_Transform* tB)
 {
-	return CollisionPoint(glm::vec3(0), glm::vec3(0), false);
+	glm::vec3 Amin = bA->Min() * tA->Scale() + tA->Position();
+	glm::vec3 Amax = bA->Max() * tA->Scale() + tA->Position();
+
+	glm::vec3 Bmin = bB->Min() * tB->Scale() + tB->Position();
+	glm::vec3 Bmax = bB->Max() * tB->Scale() + tB->Position();
+
+	glm::vec3 Acenter = { (Amin.x + Amax.x) / 2, (Amin.y + Amax.y) / 2, (Amin.z + Amax.z) / 2 };
+	glm::vec3 Bcenter = { (Amin.x + Amax.x) / 2, (Amin.y + Amax.y) / 2, (Amin.z + Amax.z) / 2 };
+
+	glm::vec3 A = Acenter;
+	glm::vec3 B = Bcenter;
+	if (A.x > Bmax.x) {
+		A.x = Bmax.x;
+	}
+	else if (A.x < Bmin.x) {
+		A.x = Bmin.x;
+	}
+	else {
+		//Nothing
+	}
+	//REDO FOR EVERY POINT IN A AND IN B
+
+	if (
+		!(Amin.x <= Bmax.x && Amax.x >= Bmin.x) &&
+		(Amin.y <= Bmax.y && Amax.y >= Bmin.y) &&
+		(Amin.z <= Bmax.z && Amax.z >= Bmin.z)
+		) {
+		return CollisionPoint(A, B, false);
+	}
+
+
+	return CollisionPoint(A, B, true);
 }
 
 CollisionPoint CollisionAlgo::FindBoxPlane(const Component::com_Box* bA, const Component::com_Transform* tA, const Component::com_Plane* pB, const Component::com_Transform* tB)
