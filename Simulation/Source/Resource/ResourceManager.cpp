1e5df24400d4d2d258dc8f4fc0c30afe341cb941
crasheds22 <30856384+crasheds22@users.noreply.github.com>
2020-09-23 18:29:13 +0800

    Resource manager (#22)
    * Manager template class
    
    * Resource manananager

---
+++ Simulation/Source/Resource/ResourceManager.cpp
@@ -0,0 +1,53 @@
+#include "Resource/ResourceManager.h"
+
+namespace Resource
+{
+    ResourceManager& ResourceManager::Instance()
+    {
+        static ResourceManager mInstance;
+
+        return mInstance;
+    }
+
+    void ResourceManager::CreateModel(std::string name, std::string filepath)
+    {
+        Model temp(filepath);
+
+        mModelManager.Create(name, temp);
+    }
+
+    void ResourceManager::CreateTexture(std::string name, std::string filepath)
+    {
+        unsigned int tex = Texture::New(filepath.c_str());
+
+        mTextureManager.Create(name, tex);
+    }
+
+    void ResourceManager::CreateShader(std::string name, std::string vertex, std::string frag, std::string geo)
+    {
+        unsigned int sha = Shader::New(vertex.c_str(), frag.c_str(), geo.length() > 0 ? geo.c_str() : nullptr);
+
+        mShaderManager.Create(name, sha);
+    }
+
+    Model* ResourceManager::RetrieveModel(std::string name)
+    {
+        return mModelManager.Retrieve(name);
+    }
+
+    unsigned int* ResourceManager::RetrieveTexture(std::string name)
+    {
+        return mTextureManager.Retrieve(name);
+    }
+
+    unsigned int* ResourceManager::RetrieveShader(std::string name)
+    {
+        return mShaderManager.Retrieve(name);
+    }
+
+    ResourceManager::ResourceManager()
+    {
+
+    }
+
+}
