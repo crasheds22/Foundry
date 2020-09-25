07cc76e40551bc092a7e84ada05ef158faaeb59a
Aaron <a.thomson22@outlook.com>
2020-09-25 19:36:18 +0800

    proper shader class

--- Simulation/Headers/Graphics/Shader.h
+++ Simulation/Headers/Graphics/Shader.h
@@ -18,37 +18,69 @@ class ShaderLoader
 {
 public:
     static unsigned int New(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr);
-    
+
     static void Use(unsigned int ID);
-
-    static void setBool (unsigned int ID, const std::string& name, bool value);
-
-    static void setInt  (unsigned int ID, const std::string& name, int value);
-
+    
+    static void setBool(unsigned int ID, const std::string& name, bool value);
+    
+    static void setInt(unsigned int ID, const std::string& name, int value);
+    
     static void setFloat(unsigned int ID, const std::string& name, float value);
+    
+    static void setVec2(unsigned int ID, const std::string& name, const glm::vec2& value);
+    static void setVec2(unsigned int ID, const std::string& name, float x, float y);
+    
+    static void setVec3(unsigned int ID, const std::string& name, const glm::vec3& value);
+    static void setVec3(unsigned int ID, const std::string& name, float x, float y, float z);
+    
+    static void setVec4(unsigned int ID, const std::string& name, const glm::vec4& value);
+    static void setVec4(unsigned int ID, const std::string& name, float x, float y, float z, float w);
+    
+    static void setMat2(unsigned int ID, const std::string& name, const glm::mat2& mat);
+    
+    static void setMat3(unsigned int ID, const std::string& name, const glm::mat3& mat);
 
-    static void setVec2 (unsigned int ID, const std::string& name, const glm::vec2& value);
-    static void setVec2 (unsigned int ID, const std::string& name, float x, float y);
-
-    static void setVec3 (unsigned int ID, const std::string& name, const glm::vec3& value);
-    static void setVec3 (unsigned int ID, const std::string& name, float x, float y, float z);
-
-    static void setVec4 (unsigned int ID, const std::string& name, const glm::vec4& value);
-    static void setVec4 (unsigned int ID, const std::string& name, float x, float y, float z, float w);
-
-    static void setMat2 (unsigned int ID, const std::string& name, const glm::mat2& mat);
-
-    static void setMat3 (unsigned int ID, const std::string& name, const glm::mat3& mat);
-
-    static void setMat4 (unsigned int ID, const std::string& name, const glm::mat4& mat);
+    static void setMat4(unsigned int ID, const std::string& name, const glm::mat4& mat);
 
     static void DeleteShader(unsigned int ID);
 
 private:
-	ShaderLoader() { };
-    ShaderLoader(const ShaderLoader& S) { };
-    ~ShaderLoader() { };
-
     static void checkCompileErrors(unsigned int shader, std::string type);
+};
+
+class Shader
+{
+public:
+    Shader(const char* vertex, const char* frag, const char* geo = nullptr);
+
+    unsigned int ID() const;
+
+    void Use();
+
+    void setBool (const std::string& name, bool value);
+
+    void setInt  (const std::string& name, int value);
+
+    void setFloat(const std::string& name, float value);
+
+    void setVec2 (const std::string& name, const glm::vec2& value);
+    void setVec2 (const std::string& name, float x, float y);
+
+    void setVec3 (const std::string& name, const glm::vec3& value);
+    void setVec3 (const std::string& name, float x, float y, float z);
+
+    void setVec4 (const std::string& name, const glm::vec4& value);
+    void setVec4 (const std::string& name, float x, float y, float z, float w);
+
+    void setMat2 (const std::string& name, const glm::mat2& mat);
+
+    void setMat3 (const std::string& name, const glm::mat3& mat);
+
+    void setMat4 (const std::string& name, const glm::mat4& mat);
+
+    void DeleteShader();
+
+private:
+    unsigned int mID;
 };
   
--- Simulation/Source/Graphics/Shader.cpp
+++ Simulation/Source/Graphics/Shader.cpp
@@ -177,3 +177,83 @@ void ShaderLoader::checkCompileErrors(unsigned int shader, std::string type)
         }
     }
 }
+
+Shader::Shader(const char* vertex, const char* frag, const char* geo)
+{
+    mID = ShaderLoader::New(vertex, frag, geo);
+}
+
+unsigned int Shader::ID() const
+{
+    return mID;
+}
+
+void Shader::Use()
+{
+    glUseProgram(mID);
+}
+
+void Shader::setBool(const std::string& name, bool value)
+{
+    glUniform1i(glGetUniformLocation(mID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string& name, int value)
+{
+    glUniform1i(glGetUniformLocation(mID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string& name, float value)
+{
+    glUniform1f(glGetUniformLocation(mID, name.c_str()), value);
+}
+
+void Shader::setVec2(const std::string& name, const glm::vec2& value)
+{
+    glUniform2fv(glGetUniformLocation(mID, name.c_str()), 1, &value[0]);
+}
+
+void Shader::setVec2(const std::string& name, float x, float y)
+{
+    glUniform2f(glGetUniformLocation(mID, name.c_str()), x, y);
+}
+
+void Shader::setVec3(const std::string& name, const glm::vec3& value)
+{
+    glUniform3fv(glGetUniformLocation(mID, name.c_str()), 1, &value[0]);
+}
+
+void Shader::setVec3(const std::string& name, float x, float y, float z)
+{
+    glUniform3f(glGetUniformLocation(mID, name.c_str()), x, y, z);
+}
+
+void Shader::setVec4(const std::string& name, const glm::vec4& value)
+{
+    glUniform4fv(glGetUniformLocation(mID, name.c_str()), 1, &value[0]);
+}
+
+void Shader::setVec4(const std::string& name, float x, float y, float z, float w)
+{
+    glUniform4f(glGetUniformLocation(mID, name.c_str()), x, y, z, w);
+}
+
+void Shader::setMat2(const std::string& name, const glm::mat2& mat)
+{
+    glUniformMatrix2fv(glGetUniformLocation(mID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void Shader::setMat3(const std::string& name, const glm::mat3& mat)
+{
+    glUniformMatrix3fv(glGetUniformLocation(mID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void Shader::setMat4(const std::string& name, const glm::mat4& mat)
+{
+    glUniformMatrix4fv(glGetUniformLocation(mID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void Shader::DeleteShader( )
+{
+    glDeleteProgram(mID);
+}
