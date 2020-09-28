03364e8232df20090f3974c287e0f26e927634a9
Aaron Thomson <a.thomson22@outlook.com>
2020-09-28 13:18:04 +0800

    Theatre dec

--- Simulation/Headers/Theatre.h
+++ Simulation/Headers/Theatre.h
@@ -1,12 +1,32 @@
 #pragma once
 
+#include "Graphics/Graphics.h"
+#include "Resource/ResourceManager.h"
+
+#include "Manager.h"
+#include "Singleton.h"
+
+#include "Stage.h"
+
+#include "Systems/SystemsManager.h"
+
 class Theatre
+	: public Singleton<Theatre>, public Manager<Stage>
 {
 public:
+	Theatre();
 
+	void PreShow();
+
+	void PlayShow();
+
+	void EndShow();
 
 private:
+	Graphics* mGraphics;
 
+	SystemsManager* mActiveSystems;
 
+	Resource::ResourceManager* mResourceManager;
 };
 
