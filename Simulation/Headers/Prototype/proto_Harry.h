95e9a3c173e588b8366032430681cec30da6c3dd
Aaron Thomson <a.thomson22@outlook.com>
2020-10-03 12:59:39 +0800

    and Harry

---
+++ Simulation/Headers/Prototype/proto_Harry.h
@@ -0,0 +1,17 @@
+#ifndef __PROTO_HARRY_H__
+#define __PROTO_HARRY_H__
+
+#include "IPrototype.h"
+
+namespace Prototype
+{
+	class Harry
+	{
+	public:
+		Harry(std::string name, Component::com_Transform transform);
+
+	private:
+	};
+}
+
+#endif
