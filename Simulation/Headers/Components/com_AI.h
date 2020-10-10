6057b5ef36df0c574a560b4290bab1a691907621
Aaron Thomson <a.thomson22@outlook.com>
2020-10-10 13:16:35 +0800

    com ai dec

---
+++ Simulation/Headers/Components/com_AI.h
@@ -0,0 +1,28 @@
+#ifndef __COM_AI_H__
+#define __COM_AI_H__
+
+#include <glm/vec3.hpp>
+
+namespace Component
+{
+	class com_AI
+	{
+	public:
+		com_AI() {};
+		com_AI(float move, float rotate, glm::vec3& position);
+
+		void NewTarget(glm::vec3 target);
+		bool TargetReached();
+
+		void Move();
+
+	private:
+		glm::vec3 mTarget;
+		glm::vec3* mPosition;
+
+		float mSpeed;
+		float mRotate;
+	};
+}
+
+#endif
