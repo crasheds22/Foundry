d72f71392d0c79cf0c1b511051b58085411ebbe3
Aaron Thomson <a.thomson22@outlook.com>
2020-10-13 11:52:58 +0800

    WIP

--- Simulation/Headers/QuadTree.h
+++ Simulation/Headers/QuadTree.h
@@ -1,7 +1,7 @@
 #ifndef __QUADTREE_H__
 #define __QUADTREE_H__
 
-#include <vector>
+#include <array>
 #include <glm/vec2.hpp>
 #include <unordered_map>
 
@@ -9,42 +9,115 @@ template<typename T>
 class Node
 {
 public:
-	Node(glm::vec2 min, glm::vec2 max, Node* parent);
-	~Node();
+	Node(glm::vec2 min, glm::vec2 max, Node* parent)
+	{
+		Parent = parent;
 
-	glm::vec2 Min;
-	glm::vec2 Max;
+		MinBound = min;
+		MaxBound = max;
 
-	T Occupants[10];
+		Children[0] = nullptr;
+		Children[1] = nullptr;
+		Children[2] = nullptr;
+		Children[3] = nullptr;
+
+		Occupants = std::array<T, 10>();
+	}
 
 	Node* Parent;
 	Node* Children[4];
+
+	glm::vec2 MinBound;
+	glm::vec2 MaxBound;
+
+	std::array<T, 10> Occupants;
 };
 
 template <typename T>
 class QuadTree
 {
 public:
-	QuadTree(glm::vec2 min, glm::vec2 max);
-	~QuadTree();
+	QuadTree(glm::vec2 min, glm::vec2 max)
+	{
+		mRoot = new Node(min, max, nullptr);
+	}
 
-	void Insert(T item, glm::vec2 pos);
-	void Update(T item, glm::vec2 pos);
+	~QuadTree()
+	{
+		if (mRoot->Children[0] != nullptr)
+		{
+			Delete(mRoot->Children[0]);
+			delete mRoot->Children[0];
+			mRoot->Children[0] = nullptr;
+		}
 
-	void Delete(T item);
+		if (mRoot->Chidlren[1] != nullptr)
+		{
+			Delete(mRoot->Children[1]);
+			delete mRoot->Children[1];
+			mRoot->Children[1] = nullptr;
+		}
+
+		if (mRoot->Children[2] != nullptr)
+		{
+			Delete(mRoot->Children[2]);
+			delete mRoot->Children[2];
+			mRoot->Children[2] = nullptr;
+		}
+
+		if (mRoot->Children[3] != nullptr)
+		{
+			Delete(mRoot->Children[3]);
+			delete mRoot->Children[3];
+			mRoot->Children[3] = nullptr;
+		}
+
+		delete mRoot;
+		mRoot = nullptr;
+	}
+
+	void Insert(T& object, glm::vec2 position)
+	{
+
+	}
 
 private:
 	Node* mRoot;
-	
-	std::unordered_map<T, Node*> mShortCut;
 
-	void Insert(Node* node, T item, glm::vec2 pos);
-	void Update(Node* node, T item, glm::vec2 pos);
+	void Delete(Node* node)
+	{
+		if (node->Children[0] != nullptr)
+		{
+			Delete(node->Children[0]);
+			delete node->Children[0];
+			node->Children[0] = nullptr;
+		}
+		if(node->Children[1] != nullptr)
+		{
+			Delete(node->Children[1]);
+			delete node->Children[1];
+			node->Children[1] = nullptr;
+		}
+		if(node->Children[2] != nullptr)
+		{
+			Delete(node->Children[2]);
+			delete node->Children[2];
+			node->Children[2] = nullptr;
+		}
+		if(node->Children[3] != nullptr)
+		{
+			Delete(node->Children[3]);
+			delete node->Children[3];
+			node->Children[3] = nullptr;
+		}
 
-	void Delete(Node* node, T item);
+		node->Parent = nullptr;
+	}
 
-	void Split();
-	void Collapse();
+	void Insert(Node* node, T& object, glm::vec2 position)
+	{
+
+	}
 };
 
 #endif
