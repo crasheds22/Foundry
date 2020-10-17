1d9dfeb88e11e5e555e6483a1fe3eb61c5087e02
Aaron Thomson <a.thomson22@outlook.com>
2020-10-17 13:36:44 +0800

    Object permanence

--- Simulation/Headers/AI/Belief.h
+++ Simulation/Headers/AI/Belief.h
@@ -1,14 +1,25 @@
 #ifndef __BELIEF_H__
 #define __BELIEF_H__
 
+#include <unordered_map>
+
+#include "ECS/Types.h"
+
+#include "Components/com_Transform.h"
+
 namespace AI
 {
 	class Belief
 	{
 	public:
+		Belief();
+
+		void Insert(ECS::Entity entity, Component::com_Transform& pos);
+		bool Find(ECS::Entity entity);
+		Component::com_Transform& Retrieve(ECS::Entity entity);
 
 	private:
-
+		std::unordered_map<ECS::Entity, Component::com_Transform&> mEncountered;
 	};
 }
 
