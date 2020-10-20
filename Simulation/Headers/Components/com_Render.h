ad6a61147fa38ac7a12ef15988335454d141c070
Frosk-Kristian <55474756+Frosk-Kristian@users.noreply.github.com>
2020-10-20 13:10:06 +0800

    com_Render.h

--- Simulation/Headers/Components/com_Render.h
+++ Simulation/Headers/Components/com_Render.h
@@ -2,20 +2,54 @@
 
 #include "Resource/ResourceManager.h"
 
+/**	\namespace Component
+ *
+ *	The namespace all Components are located in.
+ */
 namespace Component
 {
 	class com_Render
 	{
 	public:
+		/**
+		*	\brief Default constructor
+		* 
+		*	Not to be used by the user
+		* 
+		*	\post com_Render is constructed
+		*/
 		com_Render() { mModel = nullptr; mShader = nullptr; }
+		/**
+		*	\brief Constructor
+		* 
+		*	\param [in] modelName	filename of the model
+		*	\param [in] shaderName	filename of the shader
+		* 
+		*	\post com_Render is constructed
+		*/
 		com_Render(std::string modelName, std::string shaderName);
+		/**
+		*	\brief Destructor
+		* 
+		*	\post com_Render is destructed
+		*/
 		~com_Render() { mModel = nullptr; mShader = nullptr; }
 
+		/**
+		*	\brief mModel accessor
+		* 
+		*	\return mModel
+		*/
 		Model* _Model() const;
+		/**
+		*	\brief mShader accessor
+		* 
+		*	\return mShader
+		*/
 		Shader* _Shader() const;
 
 	private:
-		Model* mModel;
-		Shader* mShader;
+		Model* mModel;		/*!< Model variable*/
+		Shader* mShader;	/*!< Shader variable*/
 	};
 }
