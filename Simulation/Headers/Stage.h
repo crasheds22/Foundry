a55af39bb15aa17d7536e1a0ea3bda8cfc7b2482
Frosk-Kristian <55474756+Frosk-Kristian@users.noreply.github.com>
2020-10-20 13:58:34 +0800

    Stage.h doxygen

--- Simulation/Headers/Stage.h
+++ Simulation/Headers/Stage.h
@@ -10,23 +10,50 @@
 
 #include "Systems/Systems.h"
 
+/**
+*	\class Stage
+*
+*	\brief Class to manage game loop
+*/
 class Stage
 	: public Manager<ECS::Entity>
 {
 public:
+	/**
+	*	\brief Default constructor
+	* 
+	*	\post Stage is constructed
+	*/
 	Stage();
+	/**
+	*	\brief Destructor
+	* 
+	*	\post Stage is destructed
+	*/
 	~Stage();
 
+	/**
+	*	\brief Initialise
+	* 
+	*	Initialises system and creates entities
+	* 
+	*	\post Systems are initialised and entities are created
+	*/
 	void Init();
 
+	/**
+	*	\brief Update
+	* 
+	*	\post The render system and player system are updated
+	*/
 	void Update();
 
 private:
-	Resource::ResourceManager* mResources = nullptr;
+	Resource::ResourceManager* mResources = nullptr;	/*!< Resource manager variable*/
 
-	Component::com_Camera mCamera; 
+	Component::com_Camera mCamera;	/*!< Camera variable*/
 
-	std::shared_ptr<System::sys_PlayerControl> PlayerSys;
-	std::shared_ptr<System::sys_Render> RenderSys;
+	std::shared_ptr<System::sys_PlayerControl> PlayerSys;	/*!< Player system variable*/
+	std::shared_ptr<System::sys_Render> RenderSys;			/*!< Render system variable*/
 };
 
