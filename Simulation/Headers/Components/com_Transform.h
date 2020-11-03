839e7bcd614697e1b5f0753cde152d7f7ddda482
Aaron <a.thomson22@outlook.com>
2020-11-03 16:10:34 +0800

    com_Transform

--- Simulation/Headers/Components/com_Transform.h
+++ Simulation/Headers/Components/com_Transform.h
@@ -4,28 +4,71 @@
 
 namespace Component
 {
+	/** @class com_Transform
+	 *
+	 *	@brief Acomponent describing the transform of an entity
+	 *
+	 *	@author Aaron Thomson
+	 */
 	class com_Transform
 	{
 	public:
+		/** @brief Constructor
+		 */
 		com_Transform() {};
+		
+		/** @brief Constructor
+		 *
+		 *	@param pos Initial position
+		 *	@param rot Initial rotations
+		 *	@param scale Initial scale
+		 */
 		com_Transform(glm::vec3 pos, glm::vec3 rot, glm::vec3 scale);
 
+		/** @breif Returns the positon
+		 */
 		glm::vec3 Position() const;
+		
+		/** @brief Updates the position
+		 *
+		 *	@param position The new position value
+		 */
 		void Position(glm::vec3 position);
 
+		/** @brief Returns the rotations
+		 */
 		glm::vec3 Rotation() const;
+		
+		/** @brief Updates the rotations
+		 *
+		 *	@param rotation The new rotation values
+		 */
 		void Rotation(glm::vec3 rotation);
 
+		/** @brief Returns the scale on each axis
+		 */
 		glm::vec3 Scale() const;
+		
+		/** @brief Updates the scale on each axis
+		 *
+		 *	@param scale The new scale values
+		 */
 		void Scale(glm::vec3 scale);
 
+		/** @brief Returns the front facing vector
+		 */
 		glm::vec3 Front() const;
+		
+		/** @brief Updates the front facing vector
+		 *
+		 *	@param front The new front vector value
+		 */
 		void Front(glm::vec3 front);
 
 	private:
-		glm::vec3 mPosition{};
-		glm::vec3 mRotation{};
-		glm::vec3 mScale{};
-		glm::vec3 mFront{};
+		glm::vec3 mPosition{};	/*<! The position	*/
+		glm::vec3 mRotation{};	/*<! The rotations about each axis */
+		glm::vec3 mScale{};		/*<! The scale on each axis */
+		glm::vec3 mFront{};		/*<! The front facing vector */
 	};
 }
