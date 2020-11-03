4e0c49f7343541f5cc6b6ed0571fd1af1bfac4dc
Aaron <a.thomson22@outlook.com>
2020-11-03 16:04:01 +0800

    com_Render

--- Simulation/Headers/Components/com_Render.h
+++ Simulation/Headers/Components/com_Render.h
@@ -4,18 +4,40 @@
 
 namespace Component
 {
+	/** @class com_Render
+	 *
+	 *	@brief A render component for a model and associated shader
+	 *
+	 *	@author Aaron Thomson
+	 */
 	class com_Render
 	{
 	public:
+		/** @brief Constructor
+		 */
 		com_Render() { mModel = nullptr; mShader = nullptr; }
+		
+		/** @brief Constructor
+		 *
+		 *	@param modelName The name of the model to use
+		 *	@param shaderName The name of the shader to use
+		 */
 		com_Render(std::string modelName, std::string shaderName);
+		
+		/** @breif Destructor
+		 */
 		~com_Render() { mModel = nullptr; mShader = nullptr; }
 
+		/** @breif Returns a pointer to the model being used by this component
+		 */
 		Model* _Model() const;
+		
+		/** @brief Returns a pointer to the shader being used
+		 */
 		Shader* _Shader() const;
 
 	private:
-		Model* mModel = nullptr;
-		Shader* mShader = nullptr;
+		Model* mModel = nullptr;	/*<! A pointer to a model object */
+		Shader* mShader = nullptr;	/*<! A pointer to a shader object */
 	};
 }
