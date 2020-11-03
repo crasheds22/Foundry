5ffd624e9b8e8fe3f7edef8fae4f685464af49d3
Aaron <a.thomson22@outlook.com>
2020-11-03 16:00:25 +0800

    com_Player

--- Simulation/Headers/Components/com_Player.h
+++ Simulation/Headers/Components/com_Player.h
@@ -2,18 +2,37 @@
 
 namespace Component
 {
+	/** @class com_Player
+	 *
+	 *	@brief A component for a player controlled entity
+	 *	
+	 *	@author Aaron Thomson
+	 */
 	class com_Player
 	{
 	public:
+		/** @brief Constructor
+		 */
 		com_Player() : mMoveSpeed(0.0f), mRotateSpeed(0.0f) {};
+		
+		/** @brief Constructor
+		 *
+		 *	@param move Initial move speed
+		 *	@param rotate Initial rotation speed
+		 */
 		com_Player(float move, float rotate);
 
+		/** @brief Returns the movement speed
+		 */
 		float MoveSpeed() const;
+		
+		/** @brief Returns the rotation speed
+		 */
 		float RotateSpeed() const;
 
 	private:
-		float mMoveSpeed;
-		float mRotateSpeed;
+		float mMoveSpeed;	/*<! The players movement speed */
+		float mRotateSpeed;	/*<! The players rotation speed */
 
 	};
 }
