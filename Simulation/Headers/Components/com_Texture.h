282a4ae5ce009b6222e73633aecf69b4ec6a5714
Aaron <a.thomson22@outlook.com>
2020-11-03 16:06:21 +0800

    com_Texture

--- Simulation/Headers/Components/com_Texture.h
+++ Simulation/Headers/Components/com_Texture.h
@@ -6,17 +6,38 @@
 
 namespace Component
 {
+	/** @class com_Texture
+	 *
+	 *	@brief A component for displaying and holding a texture
+	 *
+	 *	@author Aaron Thomson
+	 */
 	class com_Texture
 	{
 	public:
+		/** @brief Constructor
+		 */
 		com_Texture() { mTexture = nullptr; }
+		
+		/** @brief Constructor 
+		 *
+		 *	@param name The name of the texture to use
+		 */
 		com_Texture(std::string name);
+		
+		/** @brief Destructor
+		 */
 		~com_Texture() { mTexture = nullptr; }
 
+		/** @brief Returns the ID of the texture 
+		 */
 		unsigned int TextureID() const;
+		
+		/** @brief Returns the name of the texture being used
+		 */
 		std::string TextureName() const;
 
 	private:
-		Texture* mTexture;
+		Texture* mTexture;	/*<! A pointer to the texture object */
 	};
 }
