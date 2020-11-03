6498f9363aae78b351ef4b42f864ac741be0a0cf
Aaron <a.thomson22@outlook.com>
2020-11-03 15:47:47 +0800

    com_affordance

--- Simulation/Headers/Components/com_Affordance.h
+++ Simulation/Headers/Components/com_Affordance.h
@@ -6,22 +6,55 @@
 
 namespace Component 
 {
+	/** @class com_Affordance
+	 *
+	 *	@brief The Affordance Component
+	 *
+	 *	@author Aaron Thomson
+	 */
 	class com_Affordance 
 	{
 	public:
+		/** @brief Constructor
+		 */
 		com_Affordance() {};
+		/** @brief Constructor
+		 *
+		 *	@param actions A list of actions for this affordance
+		 *	@param qualities A list of qualities the object would have
+		 */
 		com_Affordance(std::map<Affordance::Action, float> actions
 					 , std::map<Affordance::Quality, float> qualities);
 
+		/** @brief Returns the value of the action queried
+		 *
+		 *	@param action The action to query
+		 */
 		float Value(Affordance::Action action);
+		
+		/** @brief returns the value of the quality queried
+		 *
+		 *	@param quality The quality to query
+		 */
 		float Value(Affordance::Quality quality);
 
+		/** @brief Updates the value of the action
+		 *
+		 *	@param action The action to update
+		 *	@param value The value to update to
+		 */
 		void Update(Affordance::Action action, float value);
+
+		/** @brief Updates the value of a quality
+		 *
+		 *	@param quality The quality to update
+		 *	@param value The value to update to
+		 */
 		void Update(Affordance::Quality quality, float value);
 
 	private:
-		std::map<Affordance::Action, float>  mActionValues;
-		std::map<Affordance::Quality, float> mQualityValues;
+		std::map<Affordance::Action, float>  mActionValues;		/*<! The list of actions available */
+		std::map<Affordance::Quality, float> mQualityValues;	/*<! The list of qualities applied */
 	};
 }
 
