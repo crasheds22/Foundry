8c2313d71393454835f268cc29d923cdb0434ac3
Aaron <a.thomson22@outlook.com>
2020-11-05 14:26:18 +0800

    Texture

--- Simulation/Headers/Graphics/Texture.h
+++ Simulation/Headers/Graphics/Texture.h
@@ -2,6 +2,12 @@
 
 #include <string>
 
+/** @enum TextureType
+ *
+ *	@brief Defines the types of textures you may come across
+ *
+ *	@author Aaron Thomson
+ */
 enum class TextureType
 {
 	DIFFUSE,
@@ -12,28 +18,57 @@ enum class TextureType
 
 };
 
+/** @class Texture
+ *
+ *	@brief Loads and holds information related to a texture
+ *
+ *	@author Aaron Thomson
+ */
 class Texture
 {
 public:
+	/** @brief Constructor
+	 */
 	Texture(const char* path, TextureType type = TextureType::DIFFUSE, std::string name = "");
 
+	/** @brief Returns the texture ID
+	 */
 	unsigned int ID() const;
+	
+	/** @brief Returns the name of the texture  
+	 */
 	std::string Name() const;
+	
+	/** @brief Returns the file path to the texture
+	 */
 	std::string FilePath() const;
+	
+	/** @brief Returns the type of texture
+	 */
 	TextureType Type() const;
 
 private:
-	unsigned int mID;
-	std::string mName;
-	std::string mFilePath;
-	TextureType mType;
+	unsigned int mID;		/*!< The texture ID */
+	std::string mName;		/*!< The name of the texture */
+	std::string mFilePath;	/*!< The path to the texture */
+	TextureType mType;		/*!< The type of texture */
 };
 
+/** @class TextureLoader
+ *
+ *	@brief Static class to load a texture
+ *
+ *	@author Aaron Thomson
+ */
 class TextureLoader
 {
 public:
+	/** @brief Loads and creates a new texture
+	 */
 	static unsigned int New(const char* path);
 
+	/** @brief Flips loaded textures vertically
+	 */
 	static void FlipVertically();
 
 private:
