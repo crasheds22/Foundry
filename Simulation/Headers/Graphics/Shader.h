6bb075b8bb88b5892851c717c8972af8b3a93e28
Aaron <a.thomson22@outlook.com>
2020-11-05 14:22:59 +0800

    shader mostly

--- Simulation/Headers/Graphics/Shader.h
+++ Simulation/Headers/Graphics/Shader.h
@@ -14,29 +14,65 @@
 #include <sstream>
 #include <iostream>
 
+/** @class ShaderLoader
+ *
+ *  @brief Static class to load and compile a shader
+ *
+ *  @author Aaron Thomson
+ */
 class ShaderLoader
 {
 public:
+    /** @brief Creates a new shader from the input files and compiles it, returning the program id
+     */
     static unsigned int New(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr);
 
 private:
+    /** @brief Helper function to check the compile time errors of the shader
+     */
     static void checkCompileErrors(unsigned int shader, std::string type);
 };
 
+/** @class Shader
+ *
+ *  @brief Class used to store the data for and manipulate a shader
+ *
+ *  @author Aaron Thomson
+ */
 class Shader
 {
 public:
+    /** @brief Constructor
+     *
+     *  @param name The name of the shader
+     *  @param vertex The path to the vertex shader
+     *  @param frag The path to the fragment shader
+     *  @param geo The path to the geometry shader
+     */
     Shader(const std::string name, const char* vertex, const char* frag, const char* geo = nullptr);
 
+    /** @brief Returns the name of the shader
+     */
     std::string Name() const;
+    
+    /** @brief Returns the ID of the shader program
+     */
     unsigned int ID() const;
 
+    /** @brief Tells OpenGL to use the shader
+     */
     void Use();
 
+    /** @brief Sets a boolean uniform
+     */
     void setBool (const std::string& name, bool value);
 
+    /** @brief Sets an integer uniform value
+     */
     void setInt  (const std::string& name, int value);
 
+    /** @brief Sets a float uniform value
+     */
     void setFloat(const std::string& name, float value);
 
     void setVec2 (const std::string& name, const glm::vec2& value);
@@ -54,10 +90,12 @@ public:
 
     void setMat4 (const std::string& name, const glm::mat4& mat);
 
+    /** @brief Deletes the sahder 
+     */
     void DeleteShader();
 
 private:
-    unsigned int mID;
-    std::string mName;
+    unsigned int mID;   /*!< The ID of the shader program */
+    std::string mName;  /*!< The name of the shader */
 };
   
