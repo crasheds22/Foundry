dd4b91ca3e2ef8e8b6f3baa3923e9d973ba82f1b
Niko Subianto <niko.subianto@gmail.com>
2020-11-05 15:51:28 +0800

    Physics System

--- Simulation/Headers/Systems/sys_Physics.h
+++ Simulation/Headers/Systems/sys_Physics.h
@@ -13,15 +13,25 @@
 
 namespace System
 {
+	/** @class sys_Physics
+	 *
+	 *	@brief A system to loop through and update all entities with a physics component
+	 *
+	 *	@author Nicholas Subianto
+	 */
 	class sys_Physics : public ECS::System
 	{
 	public:
+		/** @brief Initialises the system
+		 */
 		void Init();
 
+		/** @brief Updates all the player entities in three steps, collision detection, resolution and force application
+		 */
 		void Update();
 
 	private:
-		std::vector<Collision> CollisionList;
-		Props* ref = nullptr;
+		std::vector<Collision> CollisionList;  /*!< A vector of detected, unique collision pairs to resolve */
+		Props* ref = nullptr;				   /*!< A pointer to the props class */
 	};
 }
